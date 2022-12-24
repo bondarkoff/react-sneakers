dcd03fb3edd48e1a28fa9538233d0426ee6f68de
bondarkoff <yehor.bondarkoff@gmail.com>
2022-12-24 20:15:02 +0200

    fetch mockAPI; + cart sidemenu script

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
+import React from "react";
+
 import Card from "./components/Card";
 import Header from "./Header";
 import Drawer from "./components/Drawer";
 
-const arr = [
-  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl:'img/sneakers/item-1.png'},
-  {title: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl:'img/sneakers/item-2.png'},
-  {title: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl:'img/sneakers/item-3.png'},
-  {title: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl:'img/sneakers/item-4.png'},
-];
-
-
 function App() {
+  const [items, setItems] = React.useState([])
+  const [cartItems, setCartItems] = React.useState([])
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch('https://63a71f697989ad3286ea25c4.mockapi.io/items').then(res => {
+      return res.json();
+    }).then(json => {
+      setItems(json)
+    });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems(prev => [...prev, obj])
+  }
+
   return (
-    <div className="wrapper clear">
-      <Drawer/>
-      <Header/>
+    <div className="wrapper clear"> 
+      {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)}/>}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">  
         <div className="d-flex align-center justify-between">
           <h1>Все кроссовки</h1>
@@ -23,9 +33,13 @@ function App() {
             <input placeholder="Поиск..." type="text" />
           </div>
         </div>
-        <div className="d-flex">
-          {arr.map((obj) => (
-            <Card title={obj.title} price={obj.price} imageUrl={obj.imageUrl} onClick={() => console.log(obj)}/>
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card title={item.title} 
+            price={item.price} 
+            imageUrl={item.imageUrl} 
+            onFavourite={() => console.log("Добавили в закладки")} 
+            onPlus={(obj) => onAddToCart(obj)}/>
           ))}
         </div>
       </div>
--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,27 +1,23 @@
 import styles from './Drawer.module.scss'
 
-function Drawer() {
+function Drawer({ onClose, items = [] }) {
     return (
-        <div style={{display: "none"}} className={styles.overlay}>
+        <div className={styles.overlay}>
             <div className={styles.drawer}>
-                <h2 className="d-flex justify-between mb-30">Корзина <img className={styles.removeBtn} src="/img/btn-remove.svg" alt="Remove" /></h2>
+                <h2 className="d-flex justify-between mb-30">Корзина{' '} <img onClick={onClose} className={styles.removeBtn} src="/img/btn-remove.svg" alt="Close" /></h2>
                 <div className={styles.items}>
-                    <div className={styles.cartItem}>
-                        <img className="mr-20" width={70} height={70} src="/img/sneakers/item-1.png" alt="Sneakers" />
-                        <div className="mr-20">
-                        <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                        <b>12 999 руб.</b>
+                    {items.map((obj) => (
+                        <>
+                        <div className={styles.cartItem}>
+                            <div className="cartItemImg" style={{ backgroundImage: `url(${obj.imageUrl})` }}/>
+                            <div className="mr-20">
+                                <p className="mb-5">{obj.title}</p>
+                                <b>{obj.price} руб.</b>
+                            </div>
+                            <img className={styles.removeBtn} src="/img/btn-remove.svg" alt="Remove" />
                         </div>
-                        <img className={styles.removeBtn} src="/img/btn-remove.svg" alt="Remove" />
-                    </div>
-                    <div className={styles.cartItem}>
-                        <img className="mr-20" width={70} height={70} src="/img/sneakers/item-3.png" alt="Sneakers" />
-                        <div className="mr-20">
-                        <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                        <b>12 999 руб.</b>
-                        </div>
-                        <img className={styles.removeBtn} src="/img/btn-remove.svg" alt="Remove" />
-                    </div>
+                        </>
+                    ))}
                 </div>
 
                 <div className={styles.cartTotalBlock}>
